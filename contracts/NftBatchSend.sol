844ad8a95dc2ef5fa44fa793731b4bf03d4793f7
Jayden Pung <46307126+jaydenpung@users.noreply.github.com>
2022-07-15 14:38:41 +0800

    test

--- contracts/NftBatchSend.sol
+++ contracts/NftBatchSend.sol
@@ -11,7 +11,10 @@ contract NftBatchSend {
         address tokenAddress,
         uint256[] calldata tokenIds
     ) public {
-        ERC721(tokenAddress).isApprovedForAll(from, msg.sender);
+        //ERC721(tokenAddress).setApprovalForAll(address(this), true);
+        require(from == msg.sender, "Not the same"); //just to be sure
+        require(ERC721(tokenAddress).isApprovedForAll(from, msg.sender), "Caller do not have approval for from address");
+
         for (uint256 i = 0; i < tokenIds.length; i++) {
             ERC721(tokenAddress).safeTransferFrom(
                 from,
